cd679eb23ed17d6b3d5e198396d56fb25ef6531f
QonnyMoffo <41973003+QonnyMoffo@users.noreply.github.com>
2018-08-03 17:25:18 -0400

    Probando Login de Salco
    Prueba nro2

--- login/modulos.py
+++ login/modulos.py
@@ -9,16 +9,17 @@ la cual es ingresada en uque se ingresa a una cookie
 def validateUser(user,password):
 
 	print('alguien quiere validar!')
-	url = "http://192.168.1.201:7106/soa-infra/services/default/Serv_RegistrarLogin/bpelprocess1_client_ep?WSDL"
+	url = "http://192.168.200.50:18003/soa-infra/services/RecepCiega/Serv_RegistrarLogin/bp_registrarlogin_client_ep?WSDL"
 	headers = {'content-type': 'text/xml'}
-	body = """  <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-			    	<soap:Body>
-			        	<ns1:InputPars xmlns:ns1="http://www.example.org">
-	            			<ns1:ParUsuario>""" + str(user) + """</ns1:ParUsuario>
-	            			<ns1:ParClave>""" +  str(password) + """</ns1:ParClave>
-				        </ns1:InputPars>
-				    </soap:Body>
-				</soap:Envelope>"""
+	
+	body = 	"""<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+					<soap:Body>
+							<ns1:ParEntrada xmlns:ns1="http://www.Sqm_RegistrarLogin.org">
+									<ns1:Usuario>"""+str(user)+"""</ns1:Usuario>
+									<ns1:Password>"""+str(password)+"""</ns1:Password>
+					</ns1:ParEntrada>
+				</soap:Body>
+			</soap:Envelope>"""
 
 	# respuesta al request
 	response = requests.post(url,data=body,headers=headers)
@@ -28,8 +29,8 @@ def validateUser(user,password):
 
 	# se buscan los tags, se obtienen sus textos y se guardan en un diccionario
 	myDict = {}	
+	myDict['id_conn'] = soup.find('id_conn').get_text()
 	myDict['cod_status'] = soup.find('cod_status').get_text()
 	myDict['msg_status'] = soup.find('msg_status').get_text()
-	myDict['id_conn'] = soup.find('id_conn').get_text()
 	
 	return myDict
