a31d7106de22a494dc2523c7b9f5c99784c1906c
QonnyMoffo <41973003+QonnyMoffo@users.noreply.github.com>
2018-10-11 15:57:28 -0400

    .

--- apache/myapp.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import sys
-sys.path.insert(0,"/home/fcoubuntu/proyectos/gandarillas_test1/")
-
-from myapp import app as application
-#application.secret_key = 'Add your secret key'
